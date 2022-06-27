0441d1ba1253d5449191a1de742e209d5292119b
Asdex1240 <jose.morenobrn@uanl.edu.mx>
2022-06-27 00:16:42 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'firebaseUse',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
